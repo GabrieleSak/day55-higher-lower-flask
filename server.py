ffac769b2e7c2e6bd3fe31bf18d2eb8842f55644
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2023-02-28 13:26:29 +0200

    Generate random number between 0 and  9 and create a route that can detect the number entered by user. Tell user if number is too low, too high or correct.

--- server.py
+++ server.py
@@ -4,11 +4,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+random_number = randint(0, 9)
+print(random_number)
+
 
 @app.route("/")
 def guess_number():
     return '<h1>Guess a number between 0 and 9</h1>' \
            '<img src="https://i.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.webp">'
+
+
+@app.route("/<int:number>")
+def user_guess(number):
+    if number < random_number:
+        return '<h1 style="color:red">Too low, try again!</h1>' \
+               '<img src="https://i.giphy.com/media/jD4DwBtqPXRXa/giphy.webp">'
+    elif number > random_number:
+        return '<h1 style="color:purple">Too high, try again!</h1>' \
+               '<img src="https://i.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.webp">'
+    else:
+        return '<h1 style="color:green">You found me!</h1>' \
+               '<img src="https://i.giphy.com/media/4T7e4DmcrP9du/giphy.webp">'
 
 
 if __name__ == "__main__":
